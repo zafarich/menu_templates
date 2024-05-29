3b397c3feb4d50edd7b69106c11d232bf8ff7a1c
zafarich <zqalandarov@texnomart.uz>
2024-05-29 22:53:35 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let node = document.getElementById("my-node");
+
+htmlToImage
+  .toPng(node)
+  .then(function (dataUrl) {
+    var img = new Image();
+    img.src = dataUrl;
+    img.style = "width: 100%;";
+    document.body.style = "padding: 30px;";
+    // document.body.innerHTML = "";
+    // document.body.appendChild(img);
+  })
+  .catch(function (error) {
+    console.error("oops, something went wrong!", error);
+  });
